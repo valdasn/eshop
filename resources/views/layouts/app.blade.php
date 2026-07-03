9f1047dd9785a56324e0631874cfb81501ef2429
Valdas <80822312+valdasn@users.noreply.github.com>
2026-07-03 12:29:21 +0300

    upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests script-src 'self' 'unsafe-inline' 'unsafe-eval';">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests; script-src 'self' 'unsafe-inline' 'unsafe-eval';">
     <title>VitaShop - Your Vitamin Store</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     
